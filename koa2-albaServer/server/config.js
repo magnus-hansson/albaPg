c0be254760cfb947a1f6bb514f85d1816bbb660e
magnus-hansson <magnus.hansson@fritidsresor.se>
2016-09-18 13:17:03 +0200

    change db name

--- koa2-albaServer/server/config.js
+++ koa2-albaServer/server/config.js
@@ -1,3 +1,3 @@
 export const port = process.env.PORT || 3020
-export const connexionString = 'mongodb://localhost/koa-boilerplate'
+export const connexionString = 'mongodb://localhost/orangealbatross'
 export const baseApi = 'api'
